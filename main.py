06dd28d25951859e66afd6bce54028680a6ca4b5
killgriff22 <kilgrif22033@gmail.com>
2024-08-22 01:13:08 -0400

    test

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import math
+from MultiTerm import *
 import os
 print(os.get_terminal_size())
 input("Press Enter to continue...")
-from MultiTerm import *
-import math
-Display = Screen(tuple(os.get_terminal_size()),(0,0))
+Display = Screen(tuple(os.get_terminal_size()), (0, 0))
 Display.clear()
+
 
 def rotate(origin, point, angle):
     """
@@ -21,25 +22,37 @@ def rotate(origin, point, angle):
     qy = int(qy//1)
     return qx, qy
 
-def draw_line(p1,p2):
-    x1,y1 = p1
-    x2,y2 = p2
+
+def draw_line(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
     dx = x2 - x1
     dy = y2 - y1
     steps_x = abs(dx)
     steps_y = abs(dy)
-    func = lambda x,b:(steps_y/steps_x)*x+b 
-    if steps_x == 0:
-        func = lambda x,b:b+1 if dy > 0 else b-1
-    for x in range(steps_x):
-        y = func(x,y1)
-        Display.content[int(y)][int(x+x1)] = "X"
-points = [(10,10),(20,20)]
+    if steps_x >= steps_y:
+        step = steps_x
+    else:
+        step = steps_y
+
+    x_increment = dx / step
+    y_increment = dy / step
+
+    x = x1
+    y = y1
+
+    for _ in range(step):
+        Display[int(y)][int(x)] = "X"
+        x += x_increment
+        y += y_increment
+
+
+points = [(10, 10), (20, 20)]
 draw_line(*points)
 Display.draw()
 for i in range(180):
     Display.clear()
-    points =[rotate(points[0],points[1],math.radians(i)),points[1]]
+    points = [rotate(points[0], points[1], math.radians(i)), points[1]]
     draw_line(*points)
     Display.draw()
     time.sleep(0.1)
